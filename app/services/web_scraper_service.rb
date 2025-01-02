089ee17474ddbbbcad508e694da8d8eda77a498c
Kasvit <kasvit93@gmail.com>
2025-01-02 19:19:13 +0200

    remove unused parsers

--- app/services/web_scraper_service.rb
+++ app/services/web_scraper_service.rb
@@ -1,22 +1,55 @@
 class WebScraperService
   PARSER_MAPPING = {
-    /xn--80ac9aeh6f\.xn--p1ai/ => Parsers::RanoberfParser,
-    /tl\.rulate\.ru/ => Parsers::TirulateParser
+    /xn--80ac9aeh6f\.xn--p1ai/ => '.overflow-hidden.text-base.leading-5',
+    /tl\.rulate\.ru/ => 'div.content-text'
   }.freeze
 
   def initialize(url)
     @url = url
-    @parser = select_parser(url)
+    @selector = select_selector(url)
   end
 
   def fetch_and_parse
-    @parser.fetch_and_parse(@url)
+    html = URI.open(@url)
+    raw_content = html.read
+
+    detected_encoding = CharlockHolmes::EncodingDetector.detect(raw_content)
+    content = raw_content.force_encoding(detected_encoding[:encoding])
+
+    doc = Nokogiri::HTML(content)
+    doc = decode_text(doc)
+
+    extract_content(doc)
+  rescue StandardError => e
+    raise "Parsing error: #{e.message}"
   end
 
   private
 
-  def select_parser(url)
-    parser_class = PARSER_MAPPING.find { |domain, _| url.match?(domain) }&.last
-    (parser_class || Parsers::BaseParser).new
+  def select_selector(url)
+    PARSER_MAPPING.find { |domain, _| url.match?(domain) }&.last
+  end
+
+  def extract_content(doc)
+    content = doc.at(@selector) || doc.at('body')
+    raise 'Content not found' if content.nil? || content.inner_html.blank?
+
+    decode_html_entities(content.inner_html)
+  end
+
+  def decode_html_entities(content)
+    encoder = HTMLEntities.new
+    encoder.decode(content)
+  end
+
+  def decode_text(doc)
+    doc.encoding = 'UTF-8'
+    text = doc.to_s
+    charlock = CharlockHolmes::EncodingDetector.detect(text)
+    if charlock[:encoding] && charlock[:encoding] != 'UTF-8'
+      doc.encoding = charlock[:encoding]
+      doc = Nokogiri::HTML(doc.to_s.encode('UTF-8', charlock[:encoding]))
+    end
+    doc
   end
 end
--- spec/services/web_scraper_service_spec.rb
+++ spec/services/web_scraper_service_spec.rb
@@ -1,30 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe WebScraperService do
-  describe '#select_parser' do
-    subject { described_class.new(url).send(:select_parser, url) }
+  let(:service) { described_class.new(url) }
 
-    context 'when URL is from ranoberf' do
-      let(:url) { 'https://xn--80ac9aeh6f.xn--p1ai/chapter/123' }
+  describe '#fetch_and_parse' do
+    context 'when parsing ranoberf' do
+      let(:url) { 'https://ранобэ.рф/some-chapter' }
+      let(:html_content) do
+        <<-HTML
+          <html>
+            <body>
+              <div class="overflow-hidden text-base leading-5">
+                Текст глави
+              </div>
+            </body>
+          </html>
+        HTML
+      end
 
-      it 'returns RanoberfParser instance' do
-        expect(subject).to be_an_instance_of(Parsers::RanoberfParser)
+      before do
+        allow(URI).to receive(:open).with(url).and_return(StringIO.new(html_content))
+        allow(CharlockHolmes::EncodingDetector).to receive(:detect)
+          .and_return({ encoding: 'UTF-8' })
+      end
+
+      it 'extracts content using correct selector' do
+        expect(service.fetch_and_parse).to include('Текст глави')
       end
     end
 
-    context 'when URL is from tirulate' do
-      let(:url) { 'https://tl.rulate.ru/chapter/123' }
+    context 'when parsing tirulate' do
+      let(:url) { 'https://tl.rulate.ru/some-chapter' }
+      let(:html_content) do
+        <<-HTML
+          <html>
+            <body>
+              <div class="content-text">
+                Текст глави
+              </div>
+            </body>
+          </html>
+        HTML
+      end
 
-      it 'returns TirulateParser instance' do
-        expect(subject).to be_an_instance_of(Parsers::TirulateParser)
+      before do
+        allow(URI).to receive(:open).with(url).and_return(StringIO.new(html_content))
+        allow(CharlockHolmes::EncodingDetector).to receive(:detect)
+          .and_return({ encoding: 'UTF-8' })
+      end
+
+      it 'extracts content using correct selector' do
+        expect(service.fetch_and_parse).to include('Текст глави')
       end
     end
 
-    context 'when URL is unknown' do
-      let(:url) { 'https://example.com/chapter/123' }
+    context 'when content is not found' do
+      let(:url) { 'https://example.com' }
+      let(:html_content) { '<html><body></body></html>' }
 
-      it 'returns BaseParser instance' do
-        expect(subject).to be_an_instance_of(Parsers::BaseParser)
+      before do
+        allow(URI).to receive(:open).with(url).and_return(StringIO.new(html_content))
+        allow(CharlockHolmes::EncodingDetector).to receive(:detect)
+          .and_return({ encoding: 'UTF-8' })
+      end
+
+      it 'raises an error' do
+        expect { service.fetch_and_parse }.to raise_error('Parsing error: Content not found')
+      end
+    end
+
+    context 'when encoding needs to be converted' do
+      let(:url) { 'https://tl.rulate.ru/some-chapter' }
+      let(:html_content) do
+        content = <<-HTML
+          <html>
+            <body>
+              <div class="content-text">
+                Текст глави
+              </div>
+            </body>
+          </html>
+        HTML
+        content.encode('Windows-1251')
+      end
+
+      before do
+        allow(URI).to receive(:open).with(url).and_return(StringIO.new(html_content))
+        allow(CharlockHolmes::EncodingDetector).to receive(:detect)
+          .and_return({ encoding: 'Windows-1251' })
+      end
+
+      it 'correctly converts encoding and extracts content' do
+        expect(service.fetch_and_parse).to include('Текст глави')
+      end
+    end
+
+    context 'when network error occurs' do
+      let(:url) { 'https://example.com' }
+
+      before do
+        allow(URI).to receive(:open).with(url).and_raise(OpenURI::HTTPError.new('404 Not Found', nil))
+      end
+
+      it 'raises a parsing error' do
+        expect { service.fetch_and_parse }.to raise_error(/Parsing error:/)
       end
     end
   end
